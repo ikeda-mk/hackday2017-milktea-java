5120e58683b1c9fa6d16157e0afec576b93d462b
Yoshiya-g <y.bskmelo7@gmail.com>
2017-12-09 22:21:44 +0900

    無駄な構文修正

--- demo/src/main/java/com/example/demo/PonchanResource.java
+++ demo/src/main/java/com/example/demo/PonchanResource.java
@@ -27,11 +27,10 @@ public class PonchanResource {
 	@RequestMapping("/communication")
 	public String communication(@RequestParam String word) {
 		//ユーザーが発した言葉へのレスポンス処理
-		List<String> shiritoris = Arrays.asList("しりとり");
 		boolean responseTrue = Arrays.stream(enumWords.values()).anyMatch(enumWord ->enumWord.toString().contains(word));
-		List<String> shiritoriTrue = shiritoris.stream().filter(shiritori ->word.contains(shiritori)).collect(Collectors.toList());
+		boolean shiritoriTrue = word.contains("しりとり");
 
-		if (shiritoriTrue.size() != 0) {
+		if (shiritoriTrue) {
 			return shiritoriService.shiritori(word);
 		} else if (responseTrue) {
 			return communicationService.communication(word);
