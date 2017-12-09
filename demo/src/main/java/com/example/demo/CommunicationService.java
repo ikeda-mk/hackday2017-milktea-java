f6c1f460bcf3d360f8cc4eb4c91189f042529723
Yoshiya-g <y.bskmelo7@gmail.com>
2017-12-09 17:39:07 +0900

    rename

--- demo/src/main/java/com/example/demo/CommunicationService.java
+++ demo/src/main/java/com/example/demo/CommunicationService.java
@@ -3,8 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SpeakService {
-	public String speak (String word) {
+public class CommunicationService {
+	public String communication (String word) {
 		//ここに応答処理を書いていく
 		return "test";
 	}
--- demo/src/main/java/com/example/demo/ShiritoriService.java
+++ demo/src/main/java/com/example/demo/ShiritoriService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ShiritoriService {
-	public String srtr (String word) {
+	public String shiritori (String word) {
 		//ここにしりとり処理を書いていく
 		return "test";
 	}
