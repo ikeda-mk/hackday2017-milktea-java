baee2a4d18596edae3040fba6c4a7eeb7bde0472
hirayamaasuka <hirayamaasuka@10.11.99.165>
2017-12-09 20:33:25 +0900

    まーじ

--- demo/src/main/java/com/example/demo/ShiritoriService.java
+++ demo/src/main/java/com/example/demo/ShiritoriService.java
@@ -3,19 +3,23 @@ package com.example.demo;
 import org.springframework.stereotype.Service;
 
 import jp.ne.docomo.smt.dev.common.http.AuthApiKey;
+import jp.ne.docomo.smt.dev.dialogue.Dialogue;
+import jp.ne.docomo.smt.dev.dialogue.data.DialogueResultData;
 import jp.ne.docomo.smt.dev.dialogue.param.DialogueRequestParam;
 
 @Service
 public class ShiritoriService {
 
+	// APIキー
 	private static final String API_KEY = "73556748702f69754655714146455a4b715542442f793369347a563964455a57756a4c6b777146354b5337";
-
-	// 外部サービスの URL
-	private final String url = "https://api.repl-ai.jp/v1/dialogue";
 
 	public String shiritori (String word) {
 		//ここにしりとり処理を書いていく
-		word = "あいうえお";
+		// APIKEY の設定
+		AuthApiKey.initializeAuth(API_KEY);
+
+		System.out.println(word);
+
 		// プロキシの設定
 		// プロキシを使用しない場合はコメントにしてください
 		//ProxyInfo.initializeProxy("proxyhost.co.jp", 80);
@@ -25,8 +29,15 @@ public class ShiritoriService {
 		// ユーザの発話
 		param.setUtt(word);
 
-		// APIKEY の設定
-		AuthApiKey.initializeAuth(API_KEY);
-		return "test";
+		// 雑談対話クラスの生成して、リクエストを実行する
+		Dialogue dialogue = new Dialogue();
+		DialogueResultData resultData = new DialogueResultData();
+		try {
+	 		resultData = dialogue.request(param, "ACCESS_TOKEN");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return resultData.getUtt();
 	}
 }
