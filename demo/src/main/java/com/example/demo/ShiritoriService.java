4027f2c5a1de9de4ebd9bcdfa04466ac047f3f53
hirayamaasuka <hirayamaasuka@10.11.99.165>
2017-12-09 19:38:14 +0900

    しりとり機能実装中

--- demo/src/main/java/com/example/demo/ShiritoriService.java
+++ demo/src/main/java/com/example/demo/ShiritoriService.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import jp.ne.docomo.smt.dev.common.http.AuthApiKey;
+import jp.ne.docomo.smt.dev.dialogue.param.DialogueRequestParam;
+
 @Service
 public class ShiritoriService {
 
-	private static final String API_KEY = "fcQWt03bJZ6BNq8RqZQXP25I20d5n8r4I8WlW7Pe";
+	private static final String API_KEY = "73556748702f69754655714146455a4b715542442f793369347a563964455a57756a4c6b777146354b5337";
 
 	// 外部サービスの URL
 	private final String url = "https://api.repl-ai.jp/v1/dialogue";
@@ -19,9 +22,11 @@ public class ShiritoriService {
 
 		// 雑談対話パラメータクラスを生成して、質問を設定する
 		DialogueRequestParam param = new DialogueRequestParam();
+		// ユーザの発話
+		param.setUtt(word);
 
 		// APIKEY の設定
-		AuthApiKey.initializeAuth("APIKEY");
+		AuthApiKey.initializeAuth(API_KEY);
 		return "test";
 	}
 }
